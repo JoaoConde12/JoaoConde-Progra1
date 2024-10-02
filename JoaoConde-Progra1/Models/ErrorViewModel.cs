e37bc64c9dfe64f62824b71f344d4f23a2e24f71
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-02 08:16:58 -0500

    Agregar archivos de proyecto.

---
+++ JoaoConde-Progra1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoaoConde_Progra1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
